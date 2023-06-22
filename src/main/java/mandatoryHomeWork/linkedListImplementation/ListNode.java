8059a0e0e4fe1acd35300e1948fc6ff18447562e
Bharathi <82672271+BharathiPriya1403@users.noreply.github.com>
2023-06-22 15:53:31 +0530

    Add files via upload

---
+++ src/main/java/mandatoryHomeWork/linkedListImplementation/ListNode.java
@@ -0,0 +1,17 @@
+package linkedListImplementation;
+
+public class ListNode{
+	int val;
+	ListNode next;
+	ListNode prev;
+	
+	public ListNode() {
+	}
+	
+	ListNode(int x ){
+		val=x;
+		next=null;
+		prev=null;
+	}
+	
+}
